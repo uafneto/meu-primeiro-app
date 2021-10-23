7e2f1e681260f87d4efa7e5343a7b396cc5d3e2f
Urbano Neto <uafneto@icloud.com>
2021-10-23 11:58:03 -0300

    Primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: '<router-outlet></router-outlet>'
 })
-export class AppComponent {
-  title = 'meu-primeiro-app';
+export class AppComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+    setTimeout( ()=> {
+      console.log(1);
+    }, 5000)
+  }
 }
